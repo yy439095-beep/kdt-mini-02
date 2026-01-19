7c7fb64e09cee9af19065d053ef34440cea5c0b3
woong <yy439095@gmail.com>
2026-01-19 17:51:37 +0900

    3 commit

--- src/components/filterSection/FilterSection.jsx
+++ src/components/filterSection/FilterSection.jsx
@@ -15,7 +15,19 @@ function FilterSection({ setFilter }) {
     });
   };
 
+//체크용 함수
+  const handleCheck = () => {
+    console.log("카테고리:", selectedCategory);
+    console.log("상황:", selectedSituation);
+
+    alert(
+      `선택된 값 확인\n카테고리: ${selectedCategory}\n상황: ${selectedSituation}`
+    );
+  };
+
+
   return (
+    <div>
     <section className="filter-section">
       {/* 카테고리 */}
       <div className="filter-group1">
@@ -54,6 +66,27 @@ function FilterSection({ setFilter }) {
       </div>
       
     </section>
+    {/*확인용 체크 버튼*/}
+<div>
+   <button
+        className="check-btn"
+        onClick={handleCheck}
+        disabled={!selectedCategory || !selectedSituation}
+      >
+        선택값 확인
+      </button>
+
+      {/* 기존 메뉴 추천받기 버튼 */}
+      <button
+        className="recommend-btn"
+        onClick={handleRecommend}
+        disabled={!selectedCategory || !selectedSituation}
+      >
+        메뉴 추천받기
+      </button>
+</div>
+
+</div>
   );
 }
 
